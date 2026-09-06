e1002e82171a15fc7a1309c3ed3e0fa3b9d17f6e
wkarts <57051272+wkarts@users.noreply.github.com>
2026-09-06 12:35:08 -0300

    fix(docs): invalidate stale PWA shell cache

--- docs/pwa/sw.js
+++ docs/pwa/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'connect-api-docs-pwa-v1'
+const CACHE_VERSION = 'connect-api-docs-pwa-v2'
 const STATIC_CACHE = `${CACHE_VERSION}-static`
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`
 
