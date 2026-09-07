327619545b9a6e86fca06c37262a54e36cfdbc5a
wkarts <57051272+wkarts@users.noreply.github.com>
2026-09-07 20:10:36 -0300

    feat(zapo): wire account compatibility layer

--- src/api/integrations/channel/channel.controller.ts
+++ src/api/integrations/channel/channel.controller.ts
@@ -10,7 +10,7 @@ import EventEmitter2 from 'eventemitter2';
 
 import { BusinessStartupService } from './meta/whatsapp.business.service';
 import { BaileysStartupService } from './whatsapp/whatsapp.baileys.service';
-import { ZapoExtendedStartupService } from './whatsapp/zapo.provider.extensions';
+import { ZapoAccountStartupService } from './whatsapp/zapo.provider.account.extensions';
 
 type ChannelDataType = {
   configService: ConfigService;
@@ -69,7 +69,7 @@ export class ChannelController {
     }
 
     if (instanceData.integration === Integration.WHATSAPP_ZAPO) {
-      return new ZapoExtendedStartupService(
+      return new ZapoAccountStartupService(
         data.configService,
         data.eventEmitter,
         data.prismaRepository,
